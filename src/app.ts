211c91fb13589c618e8e2b999e8c967dce194174
Gabriel <gabrielja2antunes@gmail.com>
2022-11-18 17:01:18 -0300

    feat: criando app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import ErrorHandler from './middlewares/error';
+import router from './routers';
 
 const app = express();
+app.use(express.json());
 
+app.use('/cars', router.carsRouter);
+
+app.use(ErrorHandler.handler);
 export default app;
